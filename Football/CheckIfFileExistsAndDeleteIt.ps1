6c5efd1c373a07f8f2886da904bf9a2f546a01da
Gethyn Ellis <30595485+gethynellis@users.noreply.github.com>
2022-05-05 11:05:27 +0100

    Add files via upload

---
+++ Football/CheckIfFileExistsAndDeleteIt.ps1
@@ -0,0 +1,15 @@
+﻿<#Check if a file exists and delete it#>
+<#I need to delete the CSV file if it exists otherwise it will append a whole bunch of duplicate rows
+
+This has been included as part of the master PlayerHistory Data script but I'm going seperate it out so people can 
+reference just this snippet  
+
+We set a variable $filename to be the file path, We test if it exists using Test-Path.
+
+We wrap the Test-Path call in an IF statement, if Test-Path Returns true we can delete the file#>
+
+
+$Filename = "C:\FPL\PLayerDataLoopTest.csv"
+If (Test-Path $Filename ) {
+Remove-Item -Path $Filename
+}
